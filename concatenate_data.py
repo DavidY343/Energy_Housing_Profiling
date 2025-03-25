c4812c9f87d697efb2d26417fe74759523a8d639
DAVID_ANDRES YANEZ-MARTINEZ <100451958@ALUMNOS.UC3M.ES>
2025-03-25 10:20:55 +0100

    concatenate data

---
+++ concatenate_data.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import os
+
+
+folder_path = "data/"
+
+dataframes = []
+
+for file in os.listdir(folder_path):
+    if file.endswith(".csv"):
+        file_path = os.path.join(folder_path, file)
+        df = pd.read_csv(file_path, dtype={"cups": str}, sep=";")
+        dataframes.append(df)
+
+
+merged_df = pd.concat(dataframes, ignore_index=True)
+
+output_path = "data.csv"
+merged_df.to_csv(output_path, index=False, sep=";")
+
+print("\033[92m✔ File successfully saved as data.csv\033[0m")
